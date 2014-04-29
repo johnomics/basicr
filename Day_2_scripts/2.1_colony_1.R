08e8607114c9088a4d9ef9c6e4ecb295b9c0e1ca
John Davey <johnomics@gmail.com>
2014-04-29 20:01:36 +0100

    Edits following run of course 28/29 April
    Colony section:
    - remove XLS file example
    - Use attach and detach instead of data argument
    - Remove third part with multiple data files
    
    Bugfixes:
    - remove website links
    - List slide: Change 'a' to 'one.to.ten' (hangover from old a,b,c variable names)
    - Checking input and reporting errors - remove duplication of 'concise' sqXplusX error checking example
    - Removed temperature conversion application exercise
    - Change all =T and =F to =TRUE and =FALSE

--- Day_2_scripts/2.1_colony_1.R
+++ Day_2_scripts/2.1_colony_1.R
@@ -1,22 +1,24 @@
 # Load data, order Z and plot
 colony<-read.csv("2.1_colony_trial.csv")
 colony$Z<-factor(colony$Z,c("None","Low","Medium","High"))
-plot(Count~Z,colony)
+attach(colony)
+plot(Count~Z)
 
 # Analysis of Variance
-colony.aov<-aov(Count~Z,colony)
+colony.aov<-aov(Count~Z)
 print(summary(colony.aov))
 
-#mean(colony[colony$Z=="None",]$Count)
-#mean(colony[colony$Z=="Low",]$Count)
-#mean(colony[colony$Z=="Medium",]$Count)
-#mean(colony[colony$Z=="High",]$Count)
+#mean(colony[Z=="None",]$Count)
+#mean(colony[Z=="Low",]$Count)
+#mean(colony[Z=="Medium",]$Count)
+#mean(colony[Z=="High",]$Count)
 
-#for (z in levels(colony$Z)) {
-#    print(mean(colony[colony$Z==z,]$Count))
+#for (z in levels(Z)) {
+#    print(mean(colony[Z==z,]$Count))
 #}
 
 # Calculate group means
-colony.means<-tapply(colony$Count,colony$Z,mean)
+colony.means<-tapply(Count,Z,mean)
 print(colony.means)
 barplot(colony.means)
+detach(colony)
--- Day_2_scripts/2.1_colony_2.R
+++ Day_2_scripts/2.1_colony_2.R
@@ -1,15 +1,17 @@
 # Load data, order Z and plot
-colony<-read.csv("2.1_colony_Run1Counts.csv")
+colony<-read.csv("2.1_colony_run.csv")
 colony$Z<-factor(colony$Z,c("None","Low","Medium","High"))
+attach(colony)
 
 par(oma=c(6,2,2,2))
-boxplot(Count~X*Z,colony,las=2)
+boxplot(Count~X*Z,las=2)
 
 # Analysis of Variance
-colony.aov<-aov(Count~X*Z,colony)
+colony.aov<-aov(Count~X*Z)
 print(summary(colony.aov))
 
 # Calculate group means
-colony.means<-tapply(colony$Count,list(colony$Z,colony$X),mean)
+colony.means<-tapply(Count,list(Z,X),mean)
 print(colony.means)
 barplot(colony.means,beside=TRUE)
+detach(colony)
